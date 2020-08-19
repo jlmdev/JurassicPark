e4fe6e6733e9f5bdd2db6437544e07daf9754b8c
Josh Mann <jlmann72@gmail.com>
2020-08-19 16:09:06 -0400

    Greeting, Quit Loop, Menu working

--- Program.cs
+++ Program.cs
@@ -2,11 +2,39 @@
 
 namespace JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            // Greeting
+            Console.WriteLine("Welcome to Jurassic Park.\n\n");
+
+            // initialize variables
+            var closeProgram = "n";
+
+            // While loop to continue program
+            while (closeProgram != "q")
+            {
+                // Show Menu
+                Console.WriteLine("Menu:");
+                Console.WriteLine("View");
+                Console.WriteLine("Add");
+                Console.WriteLine("Remove");
+                Console.WriteLine("Transfer");
+                Console.WriteLine("Summary");
+                Console.WriteLine("Quit");
+
+
+                // Close Program
+                closeProgram = Console.ReadLine();
+
+
+            }
+
+
+            // Say Goodbye
+            Console.WriteLine("Goodbye.");
+
+        }
     }
-  }
 }
